5f72e492335d74fcfc5c81f4c3615ad00eeb5522
Connor Kuehl <ckuehl@redhat.com>
2020-09-28 11:29:54 -0500

    Document builtin certificate modules
    Signed-off-by: Connor Kuehl <ckuehl@redhat.com>

--- src/certs/builtin/mod.rs
+++ src/certs/builtin/mod.rs
@@ -1,4 +1,9 @@
 // SPDX-License-Identifier: Apache-2.0
 
+//! Provides access to "built-in" AMD SEV ARK and ASK certificates.
+//!
+//! These are primarily offered as a convenience measure to avoid making
+//! HTTP requests to AMD's servers.
+
 pub mod naples;
 pub mod rome;
--- src/certs/builtin/rome/mod.rs
+++ src/certs/builtin/rome/mod.rs
@@ -1,4 +1,14 @@
 // SPDX-License-Identifier: Apache-2.0
 
+//! AMD's Rome certificates.
+//!
+//! Certificate provenance: https://developer.amd.com/wp-content/resources/ask_ark_rome.cert
+//!
+//! For convenience, the certificate chain has been split into individual
+//! certificates and are embedded here as byte slices.
+
+/// The public Rome ARK certificate.
 pub const ARK: &[u8] = include_bytes!("ark.cert");
+
+/// The public Rome ASK certificate.
 pub const ASK: &[u8] = include_bytes!("ask.cert");
