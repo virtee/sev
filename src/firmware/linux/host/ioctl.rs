016b42faa9e218d03b17fee2fd6b6f9bcc87fd78
Larry Dewey <larry.dewey@amd.com>
2022-10-18 12:30:37 -0500

    snp: Adding extended report features
    - Added in FFI wrappers for Snp Extended Report Features.
    - Added in user apis for interacting with the FFI.
    - Added in some unit tests and system tests for validating behavior.
    - Added certificate check per PR request.
    
    Signed-off-by: Larry Dewey <larry.dewey@amd.com>

--- src/firmware/linux/host/ioctl.rs
+++ src/firmware/linux/host/ioctl.rs
@@ -24,6 +24,8 @@ impl_const_id! {
     GetId<'_> = 8, /* GET_ID2 is 8, the deprecated GET_ID ioctl is 7 */
 
     SnpPlatformStatus = 9,
+    SnpSetExtConfig = 0xA,
+    SnpGetExtConfig = 0xB,
 }
 
 const SEV: Group = Group::new(b'S');
@@ -57,6 +59,14 @@ pub const GET_ID: Ioctl<WriteRead, &Command<GetId<'_>>> = unsafe { SEV.write_rea
 
 /// Return information about the current status and capabilities of the SEV-SNP platform.
 pub const SNP_PLATFORM_STATUS: Ioctl<WriteRead, &Command<SnpPlatformStatus>> =
+    unsafe { SEV.write_read(0) };
+
+/// Set the SNP Extended Configuration Settings.
+pub const SNP_SET_EXT_CONFIG: Ioctl<WriteRead, &Command<SnpSetExtConfig>> =
+    unsafe { SEV.write_read(0) };
+
+/// Get the SNP Extended Configuration Settings.
+pub const SNP_GET_EXT_CONFIG: Ioctl<WriteRead, &Command<SnpGetExtConfig>> =
     unsafe { SEV.write_read(0) };
 
 /// The Rust-flavored, FFI-friendly version of `struct sev_issue_cmd` which is
--- tests/api.rs
+++ tests/api.rs
@@ -1,6 +1,9 @@
 // SPDX-License-Identifier: Apache-2.0
 
 use sev::cached_chain;
+use sev::firmware::uapi::host::types::{
+    CertTable, CertTableEntry, SnpCertType, SnpConfig, SnpExtConfig, TcbVersion,
+};
 use sev::{certs::sev::Usage, firmware::uapi::host::Firmware, Build, Version};
 
 use serial_test::serial;
@@ -154,4 +157,74 @@ fn snp_platform_status() {
         status.tcb.reported_version.bootloader,
         status.state
     );
+}
+
+fn build_ext_config(cert: bool, cfg: bool) -> SnpExtConfig {
+    let test_cfg: SnpConfig = SnpConfig::new(TcbVersion::new(2, 0, 6, 39), 31);
+    let cert_table: CertTable = CertTable {
+        entries: vec![
+            CertTableEntry::new(SnpCertType::ARK, vec![1; 28]),
+            CertTableEntry::new(SnpCertType::ASK, vec![1; 28]),
+        ],
+    };
+    SnpExtConfig {
+        config: match cfg {
+            true => Some(test_cfg),
+            false => None,
+        },
+        certs: match cert {
+            true => Some(cert_table),
+            false => None,
+        },
+        certs_len: match cert {
+            true => 2,
+            false => 0,
+        },
+    }
+}
+
+#[cfg_attr(not(all(has_sev, feature = "dangerous_hw_tests")), ignore)]
+#[ignore]
+#[test]
+#[serial]
+fn snp_set_ext_config_std() {
+    let mut fw: Firmware = Firmware::open().unwrap();
+    let new_config: SnpExtConfig = build_ext_config(true, true);
+    fw.snp_set_ext_config(&new_config).unwrap();
+}
+
+#[cfg_attr(not(all(has_sev, feature = "dangerous_hw_tests")), ignore)]
+#[ignore]
+#[test]
+#[serial]
+fn snp_get_ext_config_std() {
+    let mut fw: Firmware = Firmware::open().unwrap();
+    let new_config: SnpExtConfig = build_ext_config(true, true);
+    fw.snp_set_ext_config(&new_config).unwrap();
+    let hw_config: SnpExtConfig = fw.snp_get_ext_config().unwrap();
+    assert_eq!(new_config, hw_config);
+}
+
+#[cfg_attr(not(all(has_sev, feature = "dangerous_hw_tests")), ignore)]
+#[ignore]
+#[test]
+#[serial]
+fn snp_get_ext_config_cert_only() {
+    let mut fw: Firmware = Firmware::open().unwrap();
+    let new_config: SnpExtConfig = build_ext_config(true, false);
+    fw.snp_set_ext_config(&new_config).unwrap();
+    let hw_config: SnpExtConfig = fw.snp_get_ext_config().unwrap();
+    assert_eq!(new_config, hw_config);
+}
+
+#[cfg_attr(not(all(has_sev, feature = "dangerous_hw_tests")), ignore)]
+#[ignore]
+#[test]
+#[serial]
+fn snp_get_ext_config_cfg_only() {
+    let mut fw: Firmware = Firmware::open().unwrap();
+    let new_config: SnpExtConfig = build_ext_config(false, true);
+    fw.snp_set_ext_config(&new_config).unwrap();
+    let hw_config: SnpExtConfig = fw.snp_get_ext_config().unwrap();
+    assert_eq!(new_config, hw_config);
 }
