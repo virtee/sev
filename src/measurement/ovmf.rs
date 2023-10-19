c3d570d922ea02c30c3c753c6b47eaa8672def9e
Diego Gonzalez Villalobos <Diego.GonzalezVillalobos@amd.com>
2023-10-19 18:57:47 +0000

    Adding Measurement functionality to library
    Signed-off-by: Diego Gonzalez Villalobos <Diego.GonzalezVillalobos@amd.com>

---
+++ src/measurement/ovmf.rs
@@ -0,0 +1,320 @@
+// SPDX-License-Identifier: Apache-2.0
+
+//! Operations to handle ovmf data
+use crate::error::*;
+use bincode;
+use byteorder::{ByteOrder, LittleEndian};
+use serde::Deserialize;
+use std::{
+    collections::HashMap,
+    convert::{TryFrom, TryInto},
+    fs::File,
+    io::Read,
+    path::PathBuf,
+};
+use uuid::{uuid, Uuid};
+
+/// Convert a UUID into a little endian slice
+pub fn guid_le_to_slice(guid: &str) -> Result<[u8; 16], MeasurementError> {
+    let guid = Uuid::try_from(guid)?;
+    let guid = guid.to_bytes_le();
+    let guid = guid.as_slice();
+
+    Ok(guid.try_into()?)
+}
+
+/// Types of sections declared by OVMF SEV Metadata, as appears in: https://github.com/tianocore/edk2/blob/edk2-stable202205/OvmfPkg/ResetVector/X64/OvmfSevMetadata.asm
+#[derive(Debug, Clone, Copy, PartialEq, Deserialize)]
+#[serde(into = "u8", try_from = "u8")]
+pub enum SectionType {
+    /// SNP Secure Memory
+    SnpSecMemory = 1,
+    /// SNP secret
+    SnpSecrets = 2,
+    /// CPUID
+    CPUID = 3,
+    /// SNP kernel hashes
+    SnpKernelHashes = 0x10,
+}
+
+impl From<SectionType> for u8 {
+    fn from(value: SectionType) -> u8 {
+        value as u8
+    }
+}
+
+impl TryFrom<u8> for SectionType {
+    type Error = OVMFError;
+
+    fn try_from(value: u8) -> Result<Self, OVMFError> {
+        match value {
+            1 => Ok(SectionType::SnpSecMemory),
+            2 => Ok(SectionType::SnpSecrets),
+            3 => Ok(SectionType::CPUID),
+            0x10 => Ok(SectionType::SnpKernelHashes),
+            _ => Err(OVMFError::InvalidSectionType),
+        }
+    }
+}
+
+/// OVMF SEV Metadata Section Description
+#[repr(C)]
+#[derive(Debug, Clone, Copy, Deserialize)]
+pub struct OvmfSevMetadataSectionDesc {
+    /// Guest Physical Adress
+    pub gpa: u32,
+    /// Size
+    pub size: u32,
+    /// Section Type
+    pub section_type: SectionType,
+}
+
+impl OvmfSevMetadataSectionDesc {
+    /// Generate Description from bytes
+    fn from_bytes(value: &[u8], offset: usize) -> Result<Self, MeasurementError> {
+        let value = &value[offset..offset + std::mem::size_of::<OvmfSevMetadataSectionDesc>()];
+        bincode::deserialize(value).map_err(|e| MeasurementError::BincodeError(*e))
+    }
+}
+
+/// OVMF Metadata Header
+#[repr(C)]
+#[derive(Debug, Clone, Copy, Deserialize)]
+struct OvmfSevMetadataHeader {
+    /// Header Signature
+    signature: [u8; 4],
+    /// Size
+    size: u32,
+    /// Version
+    version: u32,
+    /// Number of items
+    num_items: u32,
+}
+
+impl OvmfSevMetadataHeader {
+    /// Generate Header from bytes
+    fn from_bytes(value: &[u8], offset: usize) -> Result<Self, MeasurementError> {
+        let value = &value[offset..offset + std::mem::size_of::<OvmfSevMetadataHeader>()];
+        bincode::deserialize(value).map_err(|e| MeasurementError::BincodeError(*e))
+    }
+
+    /// Verify Header Signature
+    fn verify(&self) -> Result<(), OVMFError> {
+        let expected_signature: &[u8] = b"ASEV";
+        if !self.signature.eq(expected_signature) {
+            return Err(OVMFError::SEVMetadataVerification("signature".to_string()));
+        }
+
+        if self.version != 1 {
+            return Err(OVMFError::SEVMetadataVerification("version".to_string()));
+        }
+
+        Ok(())
+    }
+}
+
+/// OVMF Footer
+#[repr(C, packed)]
+#[derive(Debug)]
+struct OvmfFooterTableEntry {
+    /// Size
+    size: u16,
+    /// GUID
+    guid: [u8; 16],
+}
+
+impl TryFrom<&[u8]> for OvmfFooterTableEntry {
+    type Error = MeasurementError;
+
+    /// Grenerate footer from data
+    fn try_from(value: &[u8]) -> Result<OvmfFooterTableEntry, MeasurementError> {
+        // Bytes 2-17 are the GUID
+        let guid: [u8; 16] = value[2..18].try_into()?;
+        // first 2 bytes are the size
+        let size_nums: [u8; 2] = value[0..2].try_into()?;
+        let size = u16::from_le_bytes(size_nums);
+        Ok(OvmfFooterTableEntry { size, guid })
+    }
+}
+
+const FOUR_GB: u64 = 0x100000000;
+const OVMF_TABLE_FOOTER_GUID: Uuid = uuid!("96b582de-1fb2-45f7-baea-a366c55a082d");
+const SEV_HASH_TABLE_RV_GUID: Uuid = uuid!("7255371f-3a3b-4b04-927b-1da6efa8d454");
+const SEV_ES_RESET_BLOCK_GUID: Uuid = uuid!("00f771de-1a7e-4fcb-890e-68c77e2fb44e");
+const OVMF_SEV_META_DATA_GUID: Uuid = uuid!("dc886566-984a-4798-a75e-5585a7bf67cc");
+
+/// OVMF Structure
+pub struct OVMF {
+    /// OVMF data
+    data: Vec<u8>,
+    /// Table matching GUID to it's data
+    table: HashMap<Uuid, Vec<u8>>,
+    /// Metadata item description
+    metadata_items: Vec<OvmfSevMetadataSectionDesc>,
+}
+
+impl OVMF {
+    /// Generate new OVMF structure by parsing the foorter table and SEV metadata
+    pub fn new(ovmf_file: PathBuf) -> Result<Self, MeasurementError> {
+        let mut data = Vec::new();
+        let mut file = match File::open(ovmf_file) {
+            Ok(file) => file,
+            Err(e) => return Err(MeasurementError::FileError(e)),
+        };
+
+        file.read_to_end(&mut data)?;
+
+        let mut ovmf = OVMF {
+            data,
+            table: HashMap::new(),
+            metadata_items: Vec::new(),
+        };
+
+        ovmf.parse_footer_table()?;
+        ovmf.parse_sev_metadata()?;
+
+        Ok(ovmf)
+    }
+
+    /// Grab OVMF data
+    pub fn data(&self) -> &Vec<u8> {
+        &self.data
+    }
+
+    /// Calculate OVMF GPA
+    pub fn gpa(&self) -> u64 {
+        FOUR_GB - self.data.len() as u64
+    }
+
+    /// Get an item from the OVMF table
+    fn table_item(&self, guid: &Uuid) -> Option<&Vec<u8>> {
+        self.table.get(guid)
+    }
+
+    /// Get the OVMF metadata items
+    pub fn metadata_items(&self) -> &Vec<OvmfSevMetadataSectionDesc> {
+        &self.metadata_items
+    }
+
+    /// Check if the metadata items have the desired section
+    pub fn has_metadata_section(&self, section_type: SectionType) -> bool {
+        self.metadata_items()
+            .iter()
+            .any(|s| s.section_type == section_type)
+    }
+
+    /// Check that the table supports SEV hashes
+    pub fn is_sev_hashes_table_supported(&self) -> bool {
+        self.table.contains_key(&SEV_HASH_TABLE_RV_GUID)
+            && self.sev_hashes_table_gpa().unwrap_or(0) != 0
+    }
+
+    /// Get the SEV HASHES GPA
+    pub fn sev_hashes_table_gpa(&self) -> Result<u64, OVMFError> {
+        if !(self.table.contains_key(&SEV_HASH_TABLE_RV_GUID)) {
+            return Err(OVMFError::EntryMissingInTable(
+                "SEV_HASH_TABLE_RV_GUID".to_string(),
+            ));
+        }
+        Ok(self
+            .table_item(&SEV_HASH_TABLE_RV_GUID)
+            .and_then(|entry| entry.get(..4))
+            .map(|bytes| LittleEndian::read_u32(bytes) as u64)
+            .unwrap())
+    }
+
+    /// Get the SEV-ES EIP
+    pub fn sev_es_reset_eip(&self) -> Result<u32, OVMFError> {
+        if !(self.table.contains_key(&SEV_ES_RESET_BLOCK_GUID)) {
+            return Err(OVMFError::EntryMissingInTable(
+                "SEV_ES_RESET_BLOCK_GUID".to_string(),
+            ));
+        }
+
+        Ok(self
+            .table_item(&SEV_ES_RESET_BLOCK_GUID)
+            .and_then(|entry| entry.get(..4))
+            .map(LittleEndian::read_u32)
+            .unwrap())
+    }
+
+    /// Parse footer table data
+    fn parse_footer_table(&mut self) -> Result<(), MeasurementError> {
+        self.table.clear();
+        let size = self.data.len();
+        const ENTRY_HEADER_SIZE: usize = std::mem::size_of::<OvmfFooterTableEntry>();
+        //The OVMF table ends 32 bytes before the end of the firmware binary
+        let start_of_footer_table = size - 32 - ENTRY_HEADER_SIZE;
+        let footer =
+            OvmfFooterTableEntry::try_from(&self.data.as_slice()[start_of_footer_table..])?;
+
+        let expected_footer_guid = guid_le_to_slice(OVMF_TABLE_FOOTER_GUID.to_string().as_str())?;
+
+        if !footer.guid.eq(&expected_footer_guid) {
+            return Err(MeasurementError::OVMFError(OVMFError::MismatchingGUID()));
+        }
+
+        if (footer.size as usize) < ENTRY_HEADER_SIZE {
+            return Err(MeasurementError::OVMFError(OVMFError::InvalidSize(
+                "OVMF Table Footer".to_string(),
+                footer.size as usize,
+                ENTRY_HEADER_SIZE,
+            )));
+        }
+
+        let table_size = footer.size as usize - ENTRY_HEADER_SIZE;
+
+        let table_start = start_of_footer_table - table_size;
+        let table_bytes = &self.data[table_start..start_of_footer_table];
+        let mut offset = table_size;
+        while offset >= ENTRY_HEADER_SIZE {
+            let entry =
+                OvmfFooterTableEntry::try_from(&table_bytes[offset - ENTRY_HEADER_SIZE..offset])?;
+            if entry.size < ENTRY_HEADER_SIZE as u16 {
+                return Err(MeasurementError::OVMFError(OVMFError::InvalidSize(
+                    "OVMF Table Entry".to_string(),
+                    entry.size as usize,
+                    ENTRY_HEADER_SIZE,
+                )));
+            }
+            let entry_guid = Uuid::from_slice_le(&entry.guid)?;
+
+            if offset < entry.size as usize {
+                break;
+            }
+            let entry_data = &table_bytes[offset - entry.size as usize..offset - ENTRY_HEADER_SIZE];
+            self.table.insert(entry_guid, entry_data.to_vec());
+
+            offset -= entry.size as usize;
+        }
+
+        Ok(())
+    }
+
+    /// parse SEV metadata
+    fn parse_sev_metadata(&mut self) -> Result<(), MeasurementError> {
+        match self.table.get(&OVMF_SEV_META_DATA_GUID) {
+            Some(entry) => {
+                let offset_from_end = i32::from_le_bytes(entry[..4].try_into()?);
+                let header_start = self.data.len() - (offset_from_end as usize);
+                let header = OvmfSevMetadataHeader::from_bytes(self.data.as_slice(), header_start)?;
+                header.verify()?;
+                let items = &self.data[header_start + std::mem::size_of::<OvmfSevMetadataHeader>()
+                    ..header_start + header.size as usize];
+                for i in 0..header.num_items {
+                    let offset = (i as usize) * std::mem::size_of::<OvmfSevMetadataSectionDesc>();
+                    let item = OvmfSevMetadataSectionDesc::from_bytes(items, offset)?;
+                    self.metadata_items.push(item.to_owned());
+                }
+            }
+
+            None => {
+                return Err(MeasurementError::OVMFError(OVMFError::EntryMissingInTable(
+                    "OVMF_SEV_METADATA_GUID".to_string(),
+                )));
+            }
+        }
+
+        Ok(())
+    }
+}
---
+++ src/measurement/vcpu_types.rs
@@ -0,0 +1,160 @@
+// SPDX-License-Identifier: Apache-2.0
+
+//! Enum of different exisitng AMD EPYC VCPUs
+use std::{convert::TryFrom, fmt, str::FromStr};
+
+use crate::error::MeasurementError;
+
+/// All currently available QEMU VCPU types
+#[derive(Debug, Clone, Copy, PartialEq)]
+pub enum CpuType {
+    /// EPYC
+    Epyc = 0,
+    /// EPYC V1
+    EpycV1 = 1,
+    /// EPYC V2
+    EpycV2 = 2,
+    /// EPYC Indirect Branch Predictor Barrier
+    EpycIBPB = 3,
+    /// EPYC V3
+    EpycV3 = 4,
+    /// EPYC V4
+    EpycV4 = 5,
+    /// EPYC ROME
+    EpycRome = 6,
+    /// EPYC ROME V1
+    EpycRomeV1 = 7,
+    ///EPYC ROME V2
+    EpycRomeV2 = 8,
+    /// EPYC ROME V3
+    EpycRomeV3 = 9,
+    /// EPYC MILAN
+    EpycMilan = 10,
+    /// EPYC MILAN V1
+    EpycMilanV1 = 11,
+    /// EPYC MILAN V2
+    EpycMilanV2 = 12,
+    /// EPYC GENOA
+    EpycGenoa = 13,
+    /// EPYC GENOA V1
+    EpycGenoaV1 = 14,
+}
+
+impl TryFrom<u8> for CpuType {
+    type Error = MeasurementError;
+
+    fn try_from(value: u8) -> Result<Self, MeasurementError> {
+        match value {
+            0 => Ok(CpuType::Epyc),
+            1 => Ok(CpuType::EpycV1),
+            3 => Ok(CpuType::EpycIBPB),
+            4 => Ok(CpuType::EpycV3),
+            5 => Ok(CpuType::EpycV4),
+            6 => Ok(CpuType::EpycRome),
+            7 => Ok(CpuType::EpycRomeV1),
+            8 => Ok(CpuType::EpycRomeV2),
+            9 => Ok(CpuType::EpycRomeV3),
+            10 => Ok(CpuType::EpycMilan),
+            11 => Ok(CpuType::EpycMilanV1),
+            12 => Ok(CpuType::EpycMilanV2),
+            13 => Ok(CpuType::EpycGenoa),
+            14 => Ok(CpuType::EpycGenoaV1),
+            _ => Err(MeasurementError::InvalidVcpuTypeError(value.to_string())),
+        }
+    }
+}
+
+impl CpuType {
+    /// Matching CPU-Type with its CPU signature
+    pub fn sig(&self) -> i32 {
+        match self {
+            CpuType::Epyc => cpu_sig(23, 1, 2),
+            CpuType::EpycV1 => cpu_sig(23, 1, 2),
+            CpuType::EpycV2 => cpu_sig(23, 1, 2),
+            CpuType::EpycIBPB => cpu_sig(23, 1, 2),
+            CpuType::EpycV3 => cpu_sig(23, 1, 2),
+            CpuType::EpycV4 => cpu_sig(23, 1, 2),
+            CpuType::EpycRome => cpu_sig(23, 49, 0),
+            CpuType::EpycRomeV1 => cpu_sig(23, 49, 0),
+            CpuType::EpycRomeV2 => cpu_sig(23, 49, 0),
+            CpuType::EpycRomeV3 => cpu_sig(23, 49, 0),
+            CpuType::EpycMilan => cpu_sig(25, 1, 1),
+            CpuType::EpycMilanV1 => cpu_sig(25, 1, 1),
+            CpuType::EpycMilanV2 => cpu_sig(25, 1, 1),
+            CpuType::EpycGenoa => cpu_sig(25, 17, 0),
+            CpuType::EpycGenoaV1 => cpu_sig(25, 17, 0),
+        }
+    }
+}
+
+impl fmt::Display for CpuType {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        match self {
+            CpuType::Epyc => write!(f, "EPYC"),
+            CpuType::EpycV1 => write!(f, "EPYC-v1"),
+            CpuType::EpycV2 => write!(f, "EPYC-v2"),
+            CpuType::EpycIBPB => write!(f, "EPYC-IBPB"),
+            CpuType::EpycV3 => write!(f, "EPYC-v3"),
+            CpuType::EpycV4 => write!(f, "EPYC-v4"),
+            CpuType::EpycRome => write!(f, "EPYC-Rome"),
+            CpuType::EpycRomeV1 => write!(f, "EPYC-Rome-v1"),
+            CpuType::EpycRomeV2 => write!(f, "EPYC-Rome-v2"),
+            CpuType::EpycRomeV3 => write!(f, "EPYC-Rome-v3"),
+            CpuType::EpycMilan => write!(f, "EPYC-Milan"),
+            CpuType::EpycMilanV1 => write!(f, "EPYC-Milan-v1"),
+            CpuType::EpycMilanV2 => write!(f, "EPYC-Milan-v2"),
+            CpuType::EpycGenoa => write!(f, "EPYC-Genoa"),
+            CpuType::EpycGenoaV1 => write!(f, "EPYC-Genoa-v1"),
+        }
+    }
+}
+
+impl FromStr for CpuType {
+    type Err = MeasurementError;
+
+    fn from_str(s: &str) -> Result<Self, MeasurementError> {
+        match s.to_lowercase().as_str() {
+            "epyc" => Ok(CpuType::Epyc),
+            "epyc-v1" => Ok(CpuType::EpycV1),
+            "epyc-v2" => Ok(CpuType::EpycV2),
+            "epyc-ibpb" => Ok(CpuType::EpycIBPB),
+            "epyc-v3" => Ok(CpuType::EpycV3),
+            "epyc-v4" => Ok(CpuType::EpycV4),
+            "epyc-rome" => Ok(CpuType::EpycRome),
+            "epyc-rome-v1" => Ok(CpuType::EpycRomeV1),
+            "epyc-rome-v2" => Ok(CpuType::EpycRomeV2),
+            "epyc-rome-v3" => Ok(CpuType::EpycRomeV3),
+            "epyc-milan" => Ok(CpuType::EpycMilan),
+            "epyc-milan-v1" => Ok(CpuType::EpycMilanV1),
+            "epyc-milan-v2" => Ok(CpuType::EpycMilanV2),
+            "epyc-genoa" => Ok(CpuType::EpycGenoa),
+            "epyc-genoa-v1" => Ok(CpuType::EpycGenoaV1),
+            _ => Err(MeasurementError::InvalidVcpuTypeError(s.to_string())),
+        }
+    }
+}
+
+/// Compute the 32-bit CPUID signature from family, model, and stepping.
+///
+/// This computation is described in AMD's CPUID Specification, publication #25481
+/// https://www.amd.com/system/files/TechDocs/25481.pdf
+/// See section: CPUID Fn0000_0001_EAX Family, Model, Stepping Identifiers
+pub fn cpu_sig(family: i32, model: i32, stepping: i32) -> i32 {
+    let family_low;
+    let family_high;
+
+    if family > 0xf {
+        family_low = 0xf;
+        family_high = (family - 0x0f) & 0xff;
+    } else {
+        family_low = family;
+        family_high = 0;
+    }
+
+    let model_low = model & 0xf;
+    let model_high = (model >> 4) & 0xf;
+
+    let stepping_low = stepping & 0xf;
+
+    (family_high << 20) | (model_high << 16) | (family_low << 8) | (model_low << 4) | stepping_low
+}
