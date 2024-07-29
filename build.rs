ae7907f4dad9211bee16b371a4096f652702973a
Larry Dewey <larry.dewey@amd.com>
2024-07-29 16:12:02 -0500

    build: Fixing linting issues with build script
    Adding configuration options to cargo check to remove linting issues
    with the latest version of clippy.
    
    Signed-off-by: Larry Dewey <larry.dewey@amd.com>

--- build.rs
+++ build.rs
@@ -3,6 +3,11 @@
 fn main() {
     use std::path::Path;
 
+    // Add all of the possible configurations.
+    println!("cargo:rustc-check-cfg=cfg(has_sev)");
+    println!("cargo:rustc-check-cfg=cfg(has_sev_guest)");
+
+    // Check if the configuration should be set to enabled.
     if cfg!(feature = "hw_tests") || Path::new("/dev/sev").exists() {
         println!("cargo:rustc-cfg=has_sev");
     }
