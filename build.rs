a68e1cc9848ee15b37b55b0e7cede86f02f758ad
Larry Dewey <larry.dewey@amd.com>
2024-07-31 10:34:24 -0500

    build: Fixing linting issue for build.rs
    Previously we were not registering the custom flags for rustc to check.
    Adding these two lines fixes a bunch of linting issues
    
    Signed-off-by: Larry Dewey <larry.dewey@amd.com>

--- build.rs
+++ build.rs
@@ -3,6 +3,11 @@
 fn main() {
     use std::path::Path;
 
+    // Register custom cfg flags for `has_sev` and `has_sev_guest`.
+    println!("cargo:rustc-check-cfg=cfg(has_sev)");
+    println!("cargo:rustc-check-cfg=cfg(has_sev_guest)");
+
+    // If the device driver is found, set the cfg flag
     if cfg!(feature = "hw_tests") || Path::new("/dev/sev").exists() {
         println!("cargo:rustc-cfg=has_sev");
     }
