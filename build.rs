7fef6a70d913aa711be218ba648eef356d9d7af5
Larry Dewey <larry.dewey@amd.com>
2024-09-12 11:50:41 -0500

    Version: Bumping minimal supported version
    Moving to supporting 1.80.0
    
    Signed-off-by: Larry Dewey <larry.dewey@amd.com>

--- build.rs
+++ build.rs
@@ -3,6 +3,10 @@
 fn main() {
     use std::path::Path;
 
+    // Add in configurations to be checked within the code.
+    println!("cargo::rustc-check-cfg=cfg(has_sev)");
+    println!("cargo::rustc-check-cfg=cfg(has_sev_guest)");
+
     if cfg!(feature = "hw_tests") || Path::new("/dev/sev").exists() {
         println!("cargo:rustc-cfg=has_sev");
     }
